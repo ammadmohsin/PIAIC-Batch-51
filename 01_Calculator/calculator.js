3711303be280db26ca5b3ef055e4a44c9ec8150c
ammadmohsin <ammadmohsin33@gmail.com>
2023-12-26 16:35:55 +0500

    My Project

---
+++ 01_Calculator/calculator.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "Kindly enter your first no: "
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "Kindly enter your second no: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplictaion", "Division"],
+        message: "Select Operator: "
+    },
+]);
+const { numberOne, numberTwo, operator } = answers;
+if (numberOne && numberTwo && operator) {
+    let result;
+    if (operator === "Addition") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "Subtraction") {
+        result = numberOne - numberTwo;
+    }
+    else if (operator === "Division") {
+        result = numberOne / numberTwo;
+    }
+    else if (operator === "Multiplictaion") {
+        result = numberOne * numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
