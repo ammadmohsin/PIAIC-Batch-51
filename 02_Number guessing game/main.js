f0bd859745de410a424ee1a2bb7c2335912cc4bd
ammadmohsin <ammadmohsin33@gmail.com>
2023-12-28 15:37:15 +0500

    my second commit

---
+++ 02_Number guessing game/main.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+let system_generated_no = Math.floor(Math.random() * 10);
+let answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userguess",
+        message: "What is your guess?"
+    }
+]);
+if (system_generated_no === answer.userguess) {
+    console.log("Yayyyy correct answer!", "You won !!!!");
+}
+else {
+    console.log("Please try again!");
+}
+console.log("Your guess :", answer.userguess);
+console.log("System No :", system_generated_no);
